757153e19df97b49eadb3514ac525a7f7147f328
sammymarks <sammy.marks@gmail.com>
2023-11-01 10:09:37 -0500

    fixed scope

--- FoodE/src/components/main-pages/CocktailDetails.jsx
+++ FoodE/src/components/main-pages/CocktailDetails.jsx
@@ -2,24 +2,28 @@ import { useState, useEffect, useContext } from 'react'
 import { Link, useParams, useNavigate } from "react-router-dom"
 import axios from 'axios'
 
-import DataContext from '../DataContext'
-const { cocktailDetailData, setCocktailDetailData } = useContext(DataContext)
+import DataContext from '../../DataContext'
 
 
-const parseCocktailDetailData = () => {
-    const data = cocktailDetailData
-
-    const title = data.strDrink
-    const imageURL = data.strDrinkThumb
-    const videoURL = data.strVideo
-    const instructionsEng = data.strInstructions
-
-    const glass = data.strGlass
-
-}
 
 
 export default function CocktailDetails () {
+
+    const { cocktailDetailData, setCocktailDetailData } = useContext(DataContext)
+
+
+    const parseCocktailDetailData = () => {
+        const data = cocktailDetailData
+
+        const title = data.strDrink
+        const imageURL = data.strDrinkThumb
+        const videoURL = data.strVideo
+        const instructionsEng = data.strInstructions
+
+        const glass = data.strGlass
+
+    }
+
     return (
         <div className="CocktailDetails">
             <h4>CocktailDetails</h4>
--- FoodE/src/components/main-pages/CocktailSearch.jsx
+++ FoodE/src/components/main-pages/CocktailSearch.jsx
@@ -2,7 +2,15 @@ import { useState, useEffect, useContext } from 'react'
 import { Link, useParams, useNavigate } from "react-router-dom"
 import axios from 'axios'
 
+import DataContext from '../../DataContext'
+
+
+
 export default function Cocktails () {
+
+    const { cocktailDetailData, setCocktailDetailData, searchResultsData, setSearchResultsData } = useContext(DataContext)
+
+
     return (
         <div className="Cocktails">
             <h4>Container for 11 category buttons</h4>
